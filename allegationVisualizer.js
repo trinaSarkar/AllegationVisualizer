c28d6e4f29d859717ff81115d0c1ab9bd8496744
trinaSarkar <tsarkar@stanford.edu>
2017-12-05 01:57:08 -0800

    plot data first attempt

--- allegationVisualizer.js
+++ allegationVisualizer.js
@@ -23,8 +23,8 @@ function projectData(dataPoint) {
 
 function filter(name) {
   let filteredData = dataset.filter(function(d) {
-  	 return d.person == name; 
-  }
+  	 return d.person == name.innerText; 
+  });
   return filteredData; 
 }
 
@@ -33,44 +33,67 @@ function loadVisualization(error, allData) {
   if (error) throw error;
   var names = document.getElementsByClassName('name'); 
   for (var i = 0; i < names.length; i++) {
-  	drawTimeline(filter(name), name);
+  	drawTimeline(filter(names[i]), names[i]);
   }
 }
 
-function getStartPoint(d) {
+var startOffset = 0; 
 
-}
+// function getStartPoint(rect, d) {
+// 	return {
+// 		x: rect.x,
+// 		y:rect.y
+// 	}
+
+// }
 
 function getEndPoint(d) {
 
 }
 
+// function getYOffset(year) {
+// 	return year %
+// }
+
 function drawTimeline(filterData, name) {
-  var div = 
-  var rect = div.getBoundingClientRect();
-
-  let lines = svg.selectAll('line');
-  let updatedLines = lines.data(allData, d => d.id);
-
-  let enterSelection = updatedLines.enter();
-  let newLines = enterSelection.append('line') 
-  	.attr("x1", rect.x)
- 	.attr("y1", rect.y)
-    .attr("x2", rect.x + 30)
-    .attr("y2", rect.y)
-    .attr("stroke-width", 2)
-    .attr("stroke", "white")
-    .on("mouseover", function(d) {		
-            toolTip.transition()		
-                .duration(200)		
-                .style("opacity", .9);		
-            toolTip.html(d.year + "<br/>"  + d.content)	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-            })				
-    .on("mouseout", function(d) {		
-            toolTip.transition()		
-                .duration(500)		
-                .style("opacity", 0);	
-    });;
+  var firstname = name.innerText.split(" ")[0].toLowerCase();
+  var timeline = document.getElementById(firstname);
+  var rect = timeline.getBoundingClientRect();
+   
+   var yOffset = 0;
+	let lines = svg.selectAll('line.'+firstname)
+	  				 .data(filterData)
+	  				 .enter()
+	  				 .append("line");
+	  let lineAttr =  lines.attr("x1", function(d) {
+	  									if (d.type == "Allegation") return rect.x;
+	  									else if (d.type == "Achievement")return rect.x + 30;})
+						 	.attr("y1", function(d) { return rect.y + ((d.year - 1967) * 10);})
+						    .attr("x2",function(d) {
+	  									if (d.type == "Allegation") return rect.x + 30;
+	  									else if (d.type == "Achievement")return rect.x + 80;})
+						    .attr("y2", function(d) {return rect.y + ((d.year - 1967) * 10);})
+						    .attr("stroke-width", 2)
+						    .attr("stroke", function(d) {
+						    			if (d.type == "Allegation") return "red";
+						    			else if (d.type == "Achievement") return "green";
+						    })
+						    .attr('id', function(d) { return d.id})
+						    .on("mouseover", function(d) {		
+					            toolTip.transition()		
+					                .duration(200)		
+					                .style("opacity", .9);		
+					            toolTip.html(d.year + "<br/>"  + d.content)	
+					                .style("left", (d3.event.pageX) + "px")		
+					                .style("top", (d3.event.pageY - 28) + "px");	
+				            })					
+					        .on("mouseout", function(d) {		
+					            toolTip.transition()		
+					                .duration(500)		
+					                .style("opacity", 0);	
+					        });
+	
 };
+
+
+
